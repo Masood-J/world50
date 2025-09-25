df43f367867f8d7ffbf4b538e63cdba8e56fab60
Masood-J <m.masood7679@gmail.com>
2025-09-25 17:07:37 +0500

    w50: Responsive Fixes

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import Image from "next/image";
 import { Lock } from "lucide-react";
 import { IoArrowForwardOutline } from "react-icons/io5";
 import Link from "next/link";
-import HeroText from "@/components/ui/HeroText";
 import LogoAnimate from "@/components/ui/LogoAnimate";
 import { ArrowRight } from "lucide-react";
 import CursorGlow from "@/components/ui/CursorGlow";
@@ -10,51 +9,52 @@ import TextReveal from "@/components/ui/TextReveal";
 export default function Home() {
   return (
     <div
-      className={`flex flex-col min-h-screen bg-[url('/BG.png')] bg-cover bg-center `}
+      className={`flex flex-col min-h-screen bg-[url('/BG.png')] bg-cover bg-center lg:bg-[position:100%_center] `}
     >
-<div className={`hidden md:block`}>
-      <CursorGlow></CursorGlow>
-</div>
+      <div className={`hidden md:block`}>
+        <CursorGlow></CursorGlow>
+      </div>
       <header className={`z-3`}>
         <div
-          className={`flex flex-row gap-5 items-center justify-between mx-14 mt-15`}
+          className={`flex flex-row gap-5 items-center justify-between mx-4 md:mx-14 mt-15`}
         >
           <Image
             src={`Logo-Full.svg`}
             width={100}
             height={150}
             alt={`Its the top 50.`}
-
           ></Image>
           <div
-            className={`group relative z-3 rounded-full border-[#109eda] border-1 px-3 pl-4 py-1.5 flex flex-row items-center gap-5 text-inter font-normal mt-4 mr-4`}
+            className={`cursor-pointer group relative z-3 rounded-full border-[#109eda] border-1 px-3 pl-4 py-1.5 flex flex-row items-center gap-5 text-inter font-normal mt-4 mr-4`}
           >
-            <button className={`text-[#109eda]`}>Members</button>
+            <button className={`text-[#109eda] cursor-pointer`}>Members</button>
             <Lock
               className={`text-[#109eda] w-3.5 h-3.5 z-2 group-hover:text-black animation-colors duration-400 ease-in-out`}
             ></Lock>
             <div
-              className={`absolute right-0 rounded-full group-hover:bg-[#109eda] group-hover:w-9.5 w-0 h-full z-1 group-hover:transition-all group-hover:duration-200 delay-100 ease-initial`}
+              className={`absolute right-0.5 rounded-full group-hover:bg-[#109eda] group-hover:w-8.5 w-0 h-8.5 z-1 group-hover:transition-all group-hover:duration-200 delay-100 ease-initial`}
             ></div>
           </div>
         </div>
       </header>
       <main
-        className={`flex-1 grid grid-rows-[1fr_1fr_60px] grid-cols-1 h-full lg:mx-0 lg:grid-rows-[1fr_60px] lg:grid-cols-[200px_1fr_1fr_1fr_1fr] z-3`}
+        className={`flex-1 grid grid-rows-[1fr_230px_60px] grid-cols-1 h-full lg:mx-0 lg:grid-rows-[1fr_60px] lg:grid-cols-[200px_1fr_1fr_1fr_1fr] z-3`}
       >
         <div
           className={` lg:mx-0 lg:col-start-2 lg:col-span-2  lg:row-start-1 self-center mb-13`}
         >
-          <div className={`flex flex-col gap-5 ml-10 lg:ml-0 lg:ml-0`}>
-            <h2 className={`font-oswald font-light text-6xl leading-18 md:w-98`}>
+          <div className={`flex flex-col gap-8 ml-10 lg:ml-0`}>
+            <h2
+              className={`font-oswald font-light text-[42px] md:text-6xl leading-18 md:w-98`}
+            >
               Invitation-only for CEOs, Boards, & Executives
             </h2>
             <div
-              className={`group relative z-3 text-[#109eda] flex flex-row justify-center gap-3 border-1 rounded-full p-1.5 font-inter font-normal w-fit pl-3.5`}
+              className={`group relative z-3 text-[#109eda] flex flex-row justify-center gap-2 md:ml-4 border-1 rounded-full p-1 py-1.5  font-inter font-normal w-fit pl-3.5 cursor-pointer`}
             >
               <h4>Inquire About Eligibility</h4>
               <div
-                className={`z-1 absolute right-0 top-0 bottom-0 group-hover:w-9 rounded-full w-0 bg-[#109eda] group-hover:transition-all group-hover:duration-200 delay-200 ease-initial`}
+                className={`z-1 absolute right-[1px] top-[2px] bottom-0 group-hover:w-[33.5px] h-[33.5px] rounded-full w-0 bg-[#109eda] group-hover:transition-all group-hover:duration-200 delay-200 ease-initial`}
               ></div>
               <button className={`z-2`}>
                 <ArrowRight
@@ -65,18 +65,18 @@ export default function Home() {
           </div>
         </div>
         <div
-          className={` lg:mr-20 lg:col-start-4 lg:-col-end-1 lg:row-start-1 ml-0 mr-5 self-center mb-13`}
+          className={` lg:mr-20 lg:col-start-4 lg:-col-end-1 lg:row-start-1 ml-0 mr-5 self-start md:self-center mb-13`}
         >
-<TextReveal></TextReveal>
+          <TextReveal></TextReveal>
         </div>
         <div
-          className={`lg:col-start-1 lg:-col-end-1 lg:row-start-2 self-end mb-0`}
+          className={`lg:col-start-1 lg:-col-end-1 lg:row-start-2 self-start mb-0`}
         >
           <LogoAnimate></LogoAnimate>
         </div>
       </main>
       <footer
-        className={`grid grid-cols-1 lg:grid-cols-3 items-center font-inter font-light my-5 mx-14
+        className={`grid grid-cols-1 grid-rows-3 lg:grid-cols-3 items-center font-inter font-light my-5 ml-5 md:mx-14
         z-3`}
       >
         <div className={`flex flex-row gap-1 text-xs font-normal`}>
@@ -86,14 +86,14 @@ export default function Home() {
           <h3>ATLANTA</h3>
         </div>
         <div
-          className={`lg:col-start-2 lg:-col-end-1 lg:place-content-end flex flex-row gap-2 text-gray-400 text-xs`}
+          className={` lg:col-start-2 lg:-col-end-1 lg:place-content-end flex flex-col md:flex-row gap-2 text-gray-400 text-xs`}
         >
           <div className={`flex flex-row gap-6 text-[#8c8b88] `}>
             <h4>Privacy</h4>
             <h4>Terms of use</h4>
             <h4>Cookies Policy</h4>
           </div>
-          <div className={`text-[#8c8c8c]`}>
+          <div className={`text-[#8c8c8c] row-start-3`}>
             <h3>@ 2025 All rights reserved</h3>
           </div>
         </div>
--- src/components/ui/TextReveal.js
+++ src/components/ui/TextReveal.js
@@ -6,30 +6,30 @@ export default function TextReveal() {
   const quotes = [
     {
       title: `Their is nowhere on this planet like World 50.`,
-      author:`Tim Campos`,
-      desc:`CIO, Apple`
+      author: `Tim Campos`,
+      desc: `CIO, Apple`,
     },
     {
       title: `In an unpredictable world, World 50 is a 'need to have'.`,
-      author:`Greg Creed`,
-      desc:`Former CEO, YUM! Brands`
+      author: `Greg Creed`,
+      desc: `Former CEO, YUM! Brands`,
     },
     {
-      title: `You can't get this kind of collaboration anywhere else`,
-      author:`Wanda Austin`,
-      desc:`Former President & CEO, Aerospace Corporation`
+      title: `You can't get this kind of collaboration anywhere else.`,
+      author: `Wanda Austin`,
+      desc: `Former President & CEO, Aerospace Corporation`,
     },
     {
       title: `Don't sell yourself short: If invited, take the call.`,
-      author:`Doug Conant`,
-      desc:`Former CEO, Campbell Soup Company`
+      author: `Doug Conant`,
+      desc: `Former CEO, Campbell Soup Company`,
     },
   ];
 
   const [index, setIndex] = useState(0);
   const words = quotes[index].title.split(" ");
   const authorDelay = words.length * 200 + 500;
-  const descDelay=authorDelay+200;
+  const descDelay = authorDelay + 200;
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((prev) => (prev + 1) % quotes.length);
@@ -37,38 +37,60 @@ export default function TextReveal() {
     return () => clearInterval(interval);
   }, []);
 
-const [delay, setDelay] = useState(0);
+  const [delay, setDelay] = useState(0);
 
   return (
-    <div className="flex flex-col justify-center gap-3 h-[200px] px-4 overflow-hidden">
-      <div className="text-3xl md:text-5xl text-center flex flex-wrap gap-2 text-[#109eda] font-oswald font-normal">
+    <div className="flex flex-col justify-center gap-3 h- px-4 overflow-hidden">
+      <div className="relative text-3xl md:text-5xl text-center flex flex-wrap gap-2 text-[#109eda] font-oswald font-normal px-5">
+        <div className={`absolute -left-3 pl-2`}>
+          <Fade key={`open-quote-${index}`} direction="right" delay={0}>
+            <span className="">“</span>
+          </Fade>
+        </div>
         {quotes[index].title.split(" ").map((word, i) => {
-          return(
-          <Fade
-            key={`${index}-${i}`}
-            direction="right"
-            delay={i * 200} // each word gets a delay
-            className={``}
-          >
-            <span>{word}</span>
-          </Fade>)
-      })}
+          if (word === "newline") {
+            return <br key={`br-${index}-${i}`} />;
+          } else {
+            return (
+              <Fade
+                key={`${index}-${i}`}
+                direction="right"
+                delay={i * 200} // each word gets a delay
+                className={``}
+              >
+                <span>{word}</span>
+              </Fade>
+            );
+          }
+        })}
+        <Fade
+          key={`close-quote-${index}`}
+          direction="right"
+          delay={(words.length + 1) * 200}
+        >
+          <span className="text-[#109eda] absolute">”</span>
+        </Fade>
       </div>
       <div>
-
-       <div>
-         <Fade className={`font-inter font-medium text-2xl`} key={`author-${index}`} delay={authorDelay} direction="right">
-           <span className="font-semibold">{quotes[index].author}</span>
-         </Fade>
-         <Fade className={`font-inter font-light text-base`} key={`desc-${index}`} delay={descDelay} direction="right">
-           <div className="text-gray-200">{quotes[index].desc}</div>
-         </Fade>
+        <div className={`px-5`}>
+          <Fade
+            className={`font-inter font-medium text-2xl`}
+            key={`author-${index}`}
+            delay={authorDelay}
+            direction="right"
+          >
+            <span className="font-semibold">{quotes[index].author}</span>
+          </Fade>
+          <Fade
+            className={`font-inter font-light text-base`}
+            key={`desc-${index}`}
+            delay={descDelay}
+            direction="right"
+          >
+            <div className="text-gray-200">{quotes[index].desc}</div>
+          </Fade>
         </div>
-
-
-
       </div>
-
     </div>
   );
 }
